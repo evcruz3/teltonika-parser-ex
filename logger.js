d02f981308bef6c39af60a6a4d844fa98215ba33
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-04-20 11:12:47 +0800

    added timestamp for console logging

--- logger.js
+++ logger.js
@@ -1,14 +1,13 @@
 const Parser = require('./utilities/teltonika-parser');
 const binutils = require('binutils64');
 const net = require('net');
-const { parse } = require('path');
-const ByteBuffer = require("bytebuffer");
 const Devices = require('./device/devices')
 const prompt = require('prompt-sync')
 const crc16ibm = require('./utilities/crc16ibm')
 const GprsCommandPacker = require("./utilities/gprsCommandPacker")
 const fs = require('fs')
 const myRL = require("serverline")
+require('log-timestamp')
 
 class Logger{
     constructor (){
