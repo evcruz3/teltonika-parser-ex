297310e8c3be6d2aa669236ea2e51b5531acc2b6
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-05-27 18:19:39 +0800

    fixed index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+logger = require('./logger')
+mqtt = require('./mqtt')
+
+inst_logger = new logger()
+inst_mqtt = new mqtt()
--- main.js
+++
@@ -1,5 +0,0 @@
-logger = require('logger.js')
-mqtt = require('mqtt.js')
-
-inst_logger = new logger()
-inst_mqtt = new mqtt()
