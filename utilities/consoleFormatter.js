5a988b0a7772d7be6ce84684c6fe70f3c03fcc6d
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-05-27 18:04:14 +0800

    Update consoleFormatter.js

--- utilities/consoleFormatter.js
+++ utilities/consoleFormatter.js
@@ -3,7 +3,7 @@
 module.exports = function(console){
     const originalConsoleLog = console.log;
     console.log = function() {
-        args = [];
+        let args = [];
         args.push( '[' + (new Date().toLocaleString("en-US", {timeZone: "Asia/Manila"})) + '] ' );
         // Note: arguments is part of the prototype
         for( var i = 0; i < arguments.length; i++ ) {
