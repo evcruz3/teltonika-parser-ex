b8661e4ee2d4067d1141c862e23d52d32cc0e3ea
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-06 21:03:01 +0800

    Update logger.js

--- logger.js
+++ logger.js
@@ -66,7 +66,7 @@ class Logger{
 
                         let requests = this.requests[id]
                         if(requests !== undefined){
-                            requests.foreach(function(item, index, object) {
+                            requests.forEach(function(item, index, object) {
                                 let now = new Date()
                                 let diff = (now.getTime() - item.timestamp.getTime())/1000
     
