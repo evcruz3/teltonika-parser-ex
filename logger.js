d24b02e921aa5af17634ff55c52169dc415ea5ef
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-06 21:33:31 +0800

    Update logger.js

--- logger.js
+++ logger.js
@@ -109,7 +109,7 @@ class Logger{
 
                         let requests = this.requests[id]
                         if(requests !== undefined){
-                            log(requests)
+                            log("Pending requests for " + id + ": " + requests)
                             this.requests[id].forEach(function(item, index, object) {
                                 let now = new Date()
                                 let diff = (now.getTime() - item.timestamp.getTime())/1000
