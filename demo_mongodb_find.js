702951183a4c322afa5c76e623e3dfd1a92b4b4e
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-08 11:21:32 +0800

    Update demo_mongodb_find.js

--- demo_mongodb_find.js
+++ demo_mongodb_find.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  dbo.collection("customers").find({}).toArray(function(err, result) {
+  dbo.collection("AVL DATA").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
