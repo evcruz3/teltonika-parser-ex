3db9421e65045b2dd6bac9c0a535b7f8494ec56f
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-09 20:29:01 +0800

    Update demo_mongodb_find.js

--- demo_mongodb_find.js
+++ demo_mongodb_find.js
@@ -3,7 +3,7 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
+  var dbo = db.db("tft-server");
   dbo.collection("AVL DATA").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
