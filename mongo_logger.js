9b0d67082b9687a54239b3dc423e864a42f1a7b2
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-14 11:11:31 +0800

    update mongo_logger

--- mongo_logger.js
+++ mongo_logger.js
@@ -13,6 +13,10 @@ log(Array(process.stdout.rows + 1).join('\n'));
 var MongoClient = mongo.MongoClient
 var mongoUrl = "mongodb://tft-server:tft100@167.71.159.65:27017/tft-server"
 
+const db = MongoClient.connect(mongoUrl, function(err, db) {
+    if (err) throw err
+    return db.db("tft-server")
+})
 
 const host = '167.71.159.65'
 const port = '1883'
@@ -33,24 +37,32 @@ const topic = '/tft100-server/+/avlrecords'
 mqtt_client.on('connect', () => {
     log('Connected to MQTT broker')
     mqtt_client.subscribe([topic], () => {
-        log(`Subscribe to topic '${topic}'`)
+        log(`Subscribed to topic '${topic}'`)
     })
 })
 mqtt_client.on('message', (topic, payload) => {
     
     let dev_id = topic.split("/")[2]
     let json_records = JSON.parse(payload)
+    if(Array.isArray(json_records)){
+        
+            let myobjects = []
+    
+            json_records.forEach(record => {
+                myobjects.push({deviceID: dev_id, record: record})
+            })
+            //let myobj = {deviceID: dev_id, records: json_records}
+            dbo.collection("AVL DATA").insertMany(myobj, {ordered:true}, function(err, res){
+                if (err) throw err
+                log(`${myobjects.length} records inserted for deviceID ${dev_id}`)
+                // db.close()
+            })
+    }
+    else{
+        log("Received data is not an AVL record")
+    }
 
-    MongoClient.connect(mongoUrl, function(err, db) {
-        if (err) throw err
-        let dbo = db.db("tft-server")
-        let myobj = {deviceID: dev_id, records: json_records}
-        dbo.collection("AVL DATA").insertOne(myobj, function(err, res){
-            if (err) throw err
-            log("1 AVL document inserted for deviceID " + dev_id)
-            db.close()
-        })
-    })
+    
 
 })
 
