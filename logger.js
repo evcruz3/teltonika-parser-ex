d9de35183aac46ccf51ea7000d97c4aacb67d300
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-14 18:22:11 +0800

    Update logger.js

--- logger.js
+++ logger.js
@@ -248,8 +248,8 @@ function sendMessage(c, data){
     SystemMessage.write(obj, pbf);
     //SystemMessage.write(data_buffer, pbf);
     pbf.writeStringField(1, `${data.deviceId}`)
-    pbf.writeStringField(2, `${data.messageType}`)
-    pbf.writeStringField(3, `${data.messageCode}`)
+    pbf.writeVarintField(2, `${data.messageType}`)
+    pbf.writeVarintField(3, `${data.messageCode}`)
     pbf.writeStringField(4, `${data.command}`)
     pbf.writeStringField(5, `${data.parameters}`)
     data.additional_info ? pbf.writeStringField(6, `${data.additional_info}`) : ''
