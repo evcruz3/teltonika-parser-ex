9c45d041a2fabf7fea6c013eac5121ea957185d7
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-14 18:40:08 +0800

    Update logger.js

--- logger.js
+++ logger.js
@@ -256,8 +256,8 @@ function sendMessage(c, data){
 
     
     let buffer = pbf.finish();
-    log("Sending: ", data)
-    log("Sending: ", SystemMessage.read(new Pbf(buffer)))
+    console.log("Sending: ", data)
+    console.log("Sending: ", SystemMessage.read(new Pbf(buffer)))
     c.write(buffer)
 }
 
