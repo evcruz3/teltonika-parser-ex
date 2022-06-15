3d5816f1c38a896c08f025acf1b6677e868185fb
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-15 15:16:10 +0800

    Update device_publisher.js

--- device_publisher.js
+++ device_publisher.js
@@ -126,11 +126,11 @@ mqtt_client.on('message', (topic, buffer) => {
         console.log(`Search for '${regex}'`)
         
         let re = new RegExp(regex)
-        let result = re.exec(message)
+        let result = re.match(message)
         console.log("result: ", result)
         
         if(result){
-            let isLocked = result.split(":")[1]
+            let isLocked = result[0].split(":")[1]
             mqtt_client.publish(`/tft100-server/${dev_id}/isLocked`, buffer, { qos: 0, retain: true }, (error) => {
                 if (error) {
                     console.error(error)
