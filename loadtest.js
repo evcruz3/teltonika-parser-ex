2eb9ea607b5900a5d3fe12e776712a3d3b60bc71
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-16 15:44:06 +0800

    Update loadtest.js

--- loadtest.js
+++ loadtest.js
@@ -1,11 +1,13 @@
 const { spawn } = require('child_process');
 
+children = {}
 for(var i = 0; i<3; i++){
     let child = spawn('node', ['childprocess.js']);
 
     child.on('exit', function (code, signal) {
-        console.log('child process exited with ' +
+        console.log(`child process ${children[child]} exited with ` +
                     `code ${code} and signal ${signal}`);
+        del children[child]
     });
 
     child.stdout.on('data', (data) => {
@@ -15,4 +17,6 @@ for(var i = 0; i<3; i++){
     child.stderr.on('data', (data) => {
         console.error(`child stderr:\n${data}`);
     });
+
+    children[child] = i
 }
