17076526b495f0d218705bc2d82daf8759f497e8
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-16 17:05:12 +0800

    Update index.js

--- utilities/teltonika-parser/index.js
+++ utilities/teltonika-parser/index.js
@@ -24,6 +24,7 @@ class TeltonikaParser {
 
   checkIsImei() {
     let imeiLength = this._toInt(this._reader.ReadBytes(2)); 
+    console.log("imeilength: ", imeiLength)
     if (imeiLength > 0) {
       this.isImei = true;
       this.imei = this._reader.ReadBytes(imeiLength).toString();
