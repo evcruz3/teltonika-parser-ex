6455e995bc2ce3e91ed8c04a6d8f43be9162bb74
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-16 15:18:40 +0800

    Update loadtest.js

--- loadtest.js
+++ loadtest.js
@@ -1,6 +1,6 @@
 const { spawn } = require('child_process');
 
-const child = spawn('node childprocess.js');
+const child = spawn('node', ['childprocess.js']);
 
 child.on('exit', function (code, signal) {
     console.log('child process exited with ' +
