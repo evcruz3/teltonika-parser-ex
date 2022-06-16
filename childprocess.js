2a07f54bbc627e436cca7d06491e3f14004c32f7
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-16 16:37:22 +0800

    Update childprocess.js

--- childprocess.js
+++ childprocess.js
@@ -1,8 +1,52 @@
-console.log("hello world")
-let amount = randomIntFromInterval(1000,5000)
-console.log(`sleeping for ${amount} ms...`)
+const binutils = require('binutils64');
+const net = require('net');
+//const mongo = require('mongodb')
+const mqtt = require('mqtt')
+const uuid = require('uuid');
+const moment = require('moment');
+
+
+
+// console.log("hello world")
+// let amount = randomIntFromInterval(1000,5000)
+// console.log(`sleeping for ${amount} ms...`)
+// sleep(amount)
+// console.log("goodbye world")
+
+let amount = randomIntFromInterval(1000,20000)
 sleep(amount)
-console.log("goodbye world")
+
+client = new net.Socket();
+
+client.connect(49366, 'localhost', () => {
+    console.log("Created a connection to tft-server")
+})
+
+client.on('data', (message) => {     
+    console.log("Received from server: ", message, "; size: ", message.length)
+    
+});  
+// Add a 'close' event handler for the client socket 
+client.on('close', () => { 
+    console.log('connection closed'); 
+});  
+client.on('error', (err) => { 
+    console.error(err); 
+}); 
+
+let IMEI = generateIMEI()
+sendIMEI(IMEI)
+
+
+
+function sendIMEI(IMEI){
+    let sizeInt = 15
+    let size_hex = Buffer.from(sizeInt.toString(16), "hex");
+    let imei_hex = Buffer.from(IMEI.toString(16), 'hex')
+
+    let encoded_message = Buffer.concat([size_hex, imei_hex])
+    client.write(encoded_message)
+}
 
 async function sleep(time){
     await new Promise(resolve => setTimeout(resolve, 5000));
@@ -10,6 +54,10 @@ async function sleep(time){
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function generateIMEI(){
+    return randomIntFromInterval(359633109248373,359633109999999)
 }
 
 
