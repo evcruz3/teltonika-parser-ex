94b1eec3b735f366e6f6cdcf2103c9386caa8ff6
Erickson Cruz <erickson.cruz@eee.upd.edu.ph>
2022-06-16 14:38:25 +0800

    Update loadtest.js

--- loadtest.js
+++ loadtest.js
@@ -8,3 +8,10 @@ child.on('exit', function (code, signal) {
 });
 
 
+child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
+});
